7a7897994ad853fbc46df4ad4f49b34a7ae10b2a
dimafr97 <dimafr@vk.com>
2026-05-15 23:15:11 +0300

    Update cardsRegistry.js

--- static/3dlab/js/content/cardsRegistry.js
+++ static/3dlab/js/content/cardsRegistry.js
@@ -8,7 +8,46 @@
 // - сейчас не придумываем несуществующие пути;
 // - старый рабочий контент временно подключается через legacyAdapters.js.
 
+import { BLOCKS, SUBBLOCKS } from "./contentTypes.js";
+
 export const CARDS = {
-  // Пока пусто.
-  // Новые карточки будем добавлять сюда после фиксации S3-структуры.
+  test_universal_card: {
+    id: "test_universal_card",
+    title: "Тест universal viewer",
+    desc: "Тестовая карточка для проверки блоков и подблоков",
+    preview: "",
+    viewerProfile: "arch",
+    legacyType: "test",
+    legacyMeta: null,
+
+    blocks: {
+      [BLOCKS.MODEL_3D]: {
+        subblocks: {
+          [SUBBLOCKS.MODEL_3D]: {
+            model: "test-model"
+          },
+          [SUBBLOCKS.VIDEOS]: {
+            videos: [
+              "https://s3.twcstorage.ru/540d791f-86c02015-75b1-462f-b960-b855e300451a/textures/doric/v1.mp4"
+            ]
+          }
+        }
+      },
+
+      [BLOCKS.SCHEMES]: {
+        subblocks: {
+          [SUBBLOCKS.SCHEMES]: {
+            images: [
+              "https://api.apparchi.ru/?path=textures/doric/s1.jpg"
+            ]
+          },
+          [SUBBLOCKS.VIDEOS]: {
+            videos: [
+              "https://s3.twcstorage.ru/540d791f-86c02015-75b1-462f-b960-b855e300451a/textures/doric/v1.mp4"
+            ]
+          }
+        }
+      }
+    }
+  }
 };
