3ee2b37ac3f5a30a169712dafd16eb1b8160fe4d
dimafr97 <dimafr@vk.com>
2026-05-16 17:47:58 +0300

    Update universalRenderer.js

--- static/3dlab/js/universalRenderer.js
+++ static/3dlab/js/universalRenderer.js
@@ -7,6 +7,7 @@
 import { CONTENT_TYPES } from "./content/contentTypes.js";
 import { setVideoList } from "./video.js";
 import { setSchemeImages } from "./scheme.js";
+import { VIEWER_PROFILES } from "./content/viewerProfiles.js";
 
 function normalizeAssetUrl(url) {
   if (!url) return url;
@@ -39,9 +40,39 @@ export function renderUniversalContent({ card, block, subblock }) {
     return;
   }
 
-  if (type === CONTENT_TYPES.MODEL) {
-    // Пока 3D открывается через legacyOpenRef в app.js.
-    // Позже сюда переедет openModel/openInset/openRoom по viewerProfile.
-    return;
+if (type === CONTENT_TYPES.MODEL) {
+  const profile = card.viewerProfile;
+
+  switch (profile) {
+    case VIEWER_PROFILES.ARCH:
+      // TODO:
+      // open arch renderer
+      break;
+
+    case VIEWER_PROFILES.INSET:
+      // TODO:
+      // open inset renderer
+      break;
+
+    case VIEWER_PROFILES.ROOMS:
+      // TODO:
+      // open rooms renderer
+      break;
+
+    case VIEWER_PROFILES.COMPOSITION:
+      // TODO:
+      // open composition renderer
+      break;
+
+    case VIEWER_PROFILES.FIGURE:
+      // TODO:
+      // open figure renderer
+      break;
+
+    default:
+      break;
   }
+
+  return;
+}
 }
