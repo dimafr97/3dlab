1b92be002aa8489cdeab0dd8a9951e597469e013
dimafr97 <dimafr@vk.com>
2026-06-17 07:06:43 +0300

    Update contentTree.js

--- static/3dlab/js/content/contentTree.js
+++ static/3dlab/js/content/contentTree.js
@@ -50,7 +50,7 @@ export const CONTENT_TREE = {
   type: NODE_TYPES.CATEGORY,
   title: "Аудитория 1",
   desc: "Локации с дверью, стеной и ширмой с коринфской капителью",
-  preview: "content/rooms/auditorium_1/preview/cover.webm",
+  preview: "content/rooms/auditorium_1/preview/cover.webp",
   children: [
     {
   id: "drawing_interiors_auditorium_1_location_1",
